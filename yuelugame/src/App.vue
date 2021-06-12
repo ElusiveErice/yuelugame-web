21f72552def574c88002d79c05d39aabcc46a193
ElusiveEric <1436326318@qq.com>
2021-06-13 01:03:42 +0800

    使用element组件库

--- yuelugame/src/App.vue
+++ yuelugame/src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>{{ title }}</el-header>
+      <el-main><router-view /></el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      title: '岳麓游戏'
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-header{
+  background-color: #B3C0D1;
+  text-align: center;
+  line-height: 60px;
+  font-family: "微软雅黑";
 }
 </style>
